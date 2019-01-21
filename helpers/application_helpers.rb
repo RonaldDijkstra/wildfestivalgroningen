029acd089cdd2bc3e4417304418b404efbdec654
ronalddijkstra <ronald@defacto.nl>
2019-01-22 00:10:53 +0100

    Move meta helpers to application_helpers

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -14,4 +14,23 @@ module ApplicationHelpers
     )
     markdown.render(contents)
   end
+
+  # Get the website name from data
+  def website_name
+    data.site.name
+  end
+
+  # Use frontmatter or fallback for page title
+  def page_title(page)
+    frontmatter_title = page.data.title
+    if frontmatter_title
+      if frontmatter_title.is_a?(Hash) && frontmatter_title[I18n.locale]
+        [frontmatter_title.send(I18n.locale), website_name].join(" - ")
+      else
+        [frontmatter_title, website_name].join(" - ")
+      end
+    else
+      website_name
+    end
+  end
 end
