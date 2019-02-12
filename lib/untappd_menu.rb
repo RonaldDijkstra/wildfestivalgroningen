7c8f312ff43c0bd9b1ecfc0eb5fe12ff12f4db24
ronalddijkstra <ronald@defacto.nl>
2019-02-12 22:13:42 +0100

    Parse menu to yaml

---
+++ lib/untappd_menu.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "net/http"
+require "uri"
+require "yaml"
+require "json"
+
+uri = URI.parse("https://business.untappd.com/api/v1/sections/350941/items")
+request = Net::HTTP::Get.new(uri)
+request["Authorization"] = "Basic UT_API_KEY"
+
+req_options = {
+  use_ssl: uri.scheme == "https"
+}
+
+response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  http.request(request)
+end
+
+File.open("data/menu.yml", "w") do |f|
+  f.write(JSON.parse(response.body).to_yaml)
+end
